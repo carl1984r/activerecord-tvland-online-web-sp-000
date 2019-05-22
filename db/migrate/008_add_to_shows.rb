8246d4e464e0af299aa88b004ab0b6da8be48bb9
Bry Cox <carl.cc@gmail.com>
2019-05-22 06:44:23 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_to_shows.rb
+++ db/migrate/008_add_to_shows.rb
@@ -1,7 +1,9 @@
 class AddToShows < ActiveRecord::Migration[4.2]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 
 end
